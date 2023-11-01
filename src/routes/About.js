9a53dbf08c2ea2eb646baaea63d8d98eacb2f638
sridhar <87705817+sridhar4git@users.noreply.github.com>
2023-11-01 11:52:18 -0500

    Updated about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,12 +3,12 @@ import {Link} from "react-router-dom";
 
 const About =()=>{
     return (
-        <div>
-            <div className="second-module lightGrey-bg">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="center-this p-5 mt-4">
+        <section className="second-module lightGrey-bg">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="row align-items-center h-100">
+                            <div className="col">
                                 <h1 className="display-4">About us</h1>
                                 <p className="lead">The company was established in 1985 as a private firm by <b>Mr. Chintham Yadagiri.</b></p>
                                 <div className="info-contact pb-3">
@@ -18,13 +18,13 @@ const About =()=>{
                                 <Link className="btn btn-outline-dark" to="/">Read More</Link>
                             </div>
                         </div>
-                        <div className="col-md-6">
-                            <img className="img-block" src={ganeshImgBg1} alt="img"/>
-                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <img className="img-block" src={ganeshImgBg1} alt="img"/>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default About;
